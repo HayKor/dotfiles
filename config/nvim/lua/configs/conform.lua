5f1626ba5ac710823fc8821266d3805222e8c4cc
HayKor <baginian.artur@yandex.ru>
2024-06-12 13:27:24 +0300

    Updated all the files

--- config/nvim/lua/configs/conform.lua
+++ config/nvim/lua/configs/conform.lua
@@ -4,6 +4,8 @@ local options = {
         css = { "prettier" },
         html = { "prettier" },
         python = { "black" },
+        markdown = { "prettier" },
+        default = { "prettier" },
     },
 
     format_on_save = {
